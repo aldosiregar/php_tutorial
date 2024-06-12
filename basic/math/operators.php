8146efbc3add1038bb314ca5f6db3798576f4612
aldosiregar <muhammadaldorafausta@gmail.com>
2024-06-12 21:46:44 +0700

    complete materi operators

--- basic/math/operators.php
+++ basic/math/operators.php
@@ -87,4 +87,103 @@
     echo "<p>apakah 14 kurang dari sama dengan 3? : " . (14 <= 3) . "</P>"; //output : 1
     echo "<p>apakah 14 sama dengan 14? : " . (14 <=> 14) . "</P>"; //output : 0
     echo "<br><br>";
+
+    //php increment/decrement operators
+    /**
+     * php increment operators digunakan untuk melakukan inkremen
+     * 
+     * sedangkan php decrement operators digunakan untuk melakukakn decrement
+     * 
+     * ++$x :	Pre-increment	Increments $x by one, then returns $x	
+     * $x++ :	Post-increment	Returns $x, then increments $x by one	
+     * --$x : Pre-decrement	Decrements $x by one, then returns $x	
+     * $x-- : Post-decrement	Returns $x, then decrements $x by one
+     */
+
+    //php logical operator
+    /**
+     * php logical operators digunakan untuk menggabungkan statement conditional
+     * 
+     * (and) :	And,	True if both $x and $y are true	
+     * (or)  :	Or.	  True if either $x or $y is true	
+     * (xor) :	Xor,	True if either $x or $y is true, but not both	
+     * (&&)  :	And,	True if both $x and $y are true	
+     * (||)  :	Or, 	True if either $x or $y is true	
+     * (!)   :  Not,	True if $x is not true	
+     */
+    $x = true;
+    $y = false;
+    echo "<h1>php logical operator</h1>";
+    echo "<p>true dan true akan menghasilkan = ". $x && $x ."</p>";
+    echo "<p>true dan false akan menghasilkan = ". $x || $y ."</p>";
+    echo "<p>nilai terbalik dari false adalah". !$y ."</p>";
+    echo "<br><br>";
+
+    //php string operator
+    /**
+     * php memiliki 2 operator yang di design khusus untuk string 
+     * 
+     * (.)	Concatenation	: Concatenation of $txt1 and $txt2	
+     * (.=)	Concatenation : assignment,	Appends $txt2 to $txt1
+     */
+
+    //php array operators
+    /**
+     * php array operators digunakan untuk membandingkan array
+     * 
+     * (+)	  Union	      :	Union of $x and $y	
+     * (==)	  Equality	  :	Returns true if $x and $y have the same key/value pairs	
+     * (===)	Identity	  :	Returns true if $x and $y have the same key/value pairs 
+     *                      in the same order and of the same types	
+     * (!=)	  Inequality	:	Returns true if $x is not equal to $y	
+     * (<>)	  Inequality	:	Returns true if $x is not equal to $y	(xor)
+     * (!==)	Non-identity:	Returns true if $x is not identical to $y
+     */
+    $x = array(1, 2 , 4 , 8, "hello");
+    $y = array(5, "good", "array", 2, 3);
+    echo "<h1>array operators</h1>";
+    echo "<p>array x : ";
+    var_dump($x);
+    echo "</p><p>array y : ";
+    var_dump($y);
+    echo "</p>";
+    echo "<p>gabungan dari x dan y adalah: ";
+    var_dump($x + $y); //output : { [0]=> int(1) [1]=> int(2) [2]=> int(4) [3]=> int(8) [4]=> string(5) "hello" }
+    echo "</p>";
+    echo "<p>apakah x dan y memiliki nilai yang sama: ";
+    var_dump($x == $y); //output : bool(false)
+    echo "</p>";
+    echo "<p>apakah x dan y memiliki nilai dan tipe data yang sama: ";
+    var_dump($x === $y); //output : bool(false)
+    echo "</p>";
+    echo "<p>apakah x tidak sama dengan y: ";
+    var_dump($x != $y); //output : bool(true)
+    echo "</p>";
+    echo "<p>apakah x dan y adalah array yang berbeda: ";
+    var_dump($x <> $y); //output : bool(true)
+    echo "</p>";
+    echo "<p>apakah x dan y tidak sama nilai dan tipe datanya: ";
+    var_dump($x !== $y); //output : bool(true)
+    echo "</p>";
+    echo "<br><br>";
+
+    //php conditional assignment operators
+    /**
+     * php conditional assignment operators digunakan untuk meng-set nilai tergantung
+     * dari sebuah kondisi
+     * 
+     * (?:)	Ternary	        :	Returns the value of $x. The value of $x is expr2 if expr1 = TRUE.
+     *                        The value of $x is expr3 if expr1 = FALSE	
+     * (??)	Null coalescing	:	Returns the value of $x.
+     *                        The value of $x is expr1 if expr1 exists, and is not NULL.
+     *                        If expr1 does not exist, or is NULL, the value of $x is expr2.
+     *                        Introduced in PHP 7
+     */
+    $x = 5;
+    $hasil = $x < 10 ? true : false;
+    $y = $nilai ?? false;
+    echo "<h1>php conditional assignment operators</h1>";
+    echo "<p>apakah $x lebih kecil dari 10? : $hasil</p>"; //output : 1
+    echo "<p>apakah nilai y sudah terisi? : $y</p>"; //output : 0
+    echo "<br><br>";
 ?>
