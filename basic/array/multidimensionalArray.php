bf40db2ae5227370eb509ebe8046688b02aa08aa
aldosiregar <muhammadaldorafausta@gmail.com>
2024-07-02 17:24:32 +0700

    selesainya semua materi array

---
+++ basic/array/multidimensionalArray.php
@@ -0,0 +1,61 @@
+<?php
+    //php mulitidimensional array
+    /**
+     * sebagian besar tutorial array sebelumnya berkutat pada one-dimensional array
+     * 
+     * tetapi terkadang kita ingin membuat array yang lebih dari satu dimensi
+     * atau dengan kata lain adalah array didalam array
+     */
+
+    //multidimensional array
+    /**
+     * multidimensional array berisi array didalam array
+     * 
+     * php mendukung mulitdimensional array dengan dimensi apapun, seperti 2, 3, 4 dst
+     * namun array yang lebih dari 3 level lebih sulit di manage untuk sebagian besar orang
+     * 
+     * note:
+     * dimensi array mengindikasikan seberapa banyak indicies yg kita butuhkan untuk mengaksesnya
+     *  1. untuk array 2 dimensi, kita memerlukan 2 indicies array
+     *  2. untuk array 3 dimensi, kita memerlukan 3 indicies array
+     */
+
+    //two dimensional array
+    /**
+     * two dimensional array adalah array didalam array, (3 dimensi adalah array didalam array didalam array lagi)
+     */
+    $data = [
+        [
+            "model", "horse Power", "travel range"
+        ],
+        [
+            "sedan", 300, 3000
+        ],
+        [
+            "jeep", 500, 6000
+        ]
+        ];
+
+    /**
+     * untuk mengaksesnya, kita harus menambahkan indicies di sebelah indicies sebelumnya
+     * 
+     * indicies pertama untuk baris, dan indicies kedua untuk kolom
+     */
+    echo "<p>" + $data[1][2] + "</p>"; //data baris kedua kolom ketiga, yaitu data 3000 
+
+    /**
+     * jika kita menggunakan loop untuk mengakses datanya, kita bisa menggunakan nested loop untuk
+     * mengakses semua datanya
+     */
+    for($row = 0; $row < count($data); $row++){
+        echo "<p>";
+        for($col = 0; $col < count($data[0]); $col++){
+            echo $data[$row][$col] + " ";
+        }
+        echo "</p>";
+    };
+
+    /**
+     * array 3 dimensi juga mirip dengan ini, jadi tutorialnya silahkan pikirkan sendiri
+     */
+?>
---
+++ basic/array/sortingArray.php
@@ -0,0 +1,64 @@
+<?php
+    /**
+     * element dari array dapat di urutkan menurut alphabets atau numerical order, ascending atau descending
+     */
+
+    //sort function untuk arrays
+    /**
+     * beberapa function yang bisa digunakan untuk mengurutkan arrays adalah sebagai berikut :
+     * sort() - sort arrays in ascending order
+     * rsort() - sort arrays in descending order
+     * asort() - sort associative arrays in ascending order, according to the value
+     * ksort() - sort associative arrays in ascending order, according to the key
+     * arsort() - sort associative arrays in descending order, according to the value
+     * krsort() - sort associative arrays in descending order, according to the key
+     */
+
+    //sort function
+    /**
+     * sort function mengurutkan array secara urutan ascending (kecil ke besar)
+     */
+    $data = [1,3, 5 ,2, 7, 0];
+    echo "<p>" + sort($data) + "</p>";
+
+    //rsort function
+    /**
+     * rsort function adalah kebalikan dari sort, yaitu urutannya descending (besar ke kecil)
+     */
+    echo "<p>" + rsort($data) + "</p>";
+
+    //asort() function
+    /**
+     * asort function mengurukan associative array dalam urutan ascending, tergantung dari nilainya
+     */
+    $personal = [
+        "umur" => 31,
+        "nama" => "aldo",
+        "jabatan" => "it developer",
+        "jarak" => 2000
+    ];
+    var_dump(asort($personal));
+    echo "<br>";
+
+    //ksort function
+    /**
+     * ksort bekerja sama seperti asort, bedanya urutannya dilihat dari urutan keynya 
+     * (jika angka diurutkan dari besar angkanya, jika huruf dilihat dari encoding karakternya)
+     */
+    var_dump(ksort($personal));
+    echo "<br>";
+
+    //arsort() function
+    /**
+     * arsort bekerja seperti asort, tetapi urutannya descending
+     */
+    var_dump(arsort($personal));
+    echo "<br>";
+
+    //krsort() function
+    /**
+     * krsotr bekerja seperti ksort, tetapi urutannya descending
+     */
+    var_dump(krsort($personal));
+    echo "<br>";
+?>
